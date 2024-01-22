e6fbb508dccb8bcee766fb24a1f460ffb3302380
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2024-01-22 12:24:00 +0330

    the first commit

---
+++ Variable_Spelling.py
@@ -0,0 +1,16 @@
+#type Varibale
+a1=5
+a2=5.5
+a3="5"
+a4=True
+
+#print("The type of a1 is:",a1, type(a1)) int
+#print("The type of a2 is:",a2, type(a2)) float
+#print("The type of a3 is:",a3,type(a3))  string
+#print("The type of a4 is:",a4, type(a4)) bool
+b=a1+a2
+print("The type of b is:",b, type(b))
+c=a3+a3
+print("The type of c is:",c, type(c))
+d=a1+a4
+print("The type of d is:",d, type(d))
---
+++ first_project.py
@@ -0,0 +1,2 @@
+#The first Project
+print("Hello my Students, I will  happy to teach")
