ef9a34c6f49efbebcfaf62a99a91d3a7ab946d5a
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2024-02-02 23:43:32 +0330

    if and variable

---
+++ IF_samples.py
@@ -0,0 +1,13 @@
+#If Explanition
+x=int(input("Please insert your number"))
+a=2
+b=6
+if( x>a and x<b):
+    print("the result is:  ",1)
+elif(x<=a):
+    #print("the result is: ",2)
+    pass
+elif (x>=b):
+    print("The result is: ",3)
+else:
+    print("Your result is amzing")
